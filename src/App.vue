64ad24565991311960a14f7ceadd302f3df02cee
Arapu Trevor <105975921+Trevor-143@users.noreply.github.com>
2023-02-09 13:54:25 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,101 @@
+<template>
+  <header>
+    <div class="top">
+      <img src="./assets/bleach.jpg" alt="cover image">
+      <div class="inside">
+        <h2>Lets Go</h2>
+        <p>Here, you'll find everything you need to 
+          know about your favorite anime shows and characters.
+           So why wait? Start exploring the world of anime with us today!</p>
+        <form>
+          <input id="search_text" type="text" placeholder="search and...">
+          <button id="search_button">Find</button>
+        </form>
+      </div>
+    </div>
+  </header>
+  <router-view/>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'poppins', sans-serif;
+}
+header {
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.top {
+  position: relative;
+  max-width: 1000px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 380px;
+  border-radius: 20px;
+  overflow: hidden;
+}
+.top img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+.inside {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.7);
+  width: 100%;
+  height: 100%;
+  color: #fff;
+}
+.inside p {
+  max-width: 650px;
+  text-align: center;
+  padding: 10px 0;
+}
+form {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background: #fff;
+  padding: 10px;
+  border-radius: 10px;
+  color: #000000;
+  width: 70%;
+}
+form input {
+  background: none;
+  border: none;
+  width: 100%;
+}
+
+form input:focus {
+  background: none;
+  outline: none;
+}
+
+form button {
+  transition: 0.5s;
+  background: #000000;
+  color: #fff;
+  padding: 7px 15px;
+  border-radius: 10px;
+  border: 2px solid #000000;
+  cursor: pointer;
+}
+form button:hover {
+  color: #000000;
+  background: #fff;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script>
+import { ref, onBeforeMount } from "vue";
+
+export default {
+  name: 'HomeView',
+  setup() {
+    const shows = ref([])
+
+    onBeforeMount (() => {
+      fetch(`https://api.jikan.moe/v4/anime/genre/14/Horror`)
+      .then(response => response.json())
+      .then(data => {
+        shows.value = data
+        console.log(shows.value)
+      })
+    })    
+  }
+  
+}
+</script>
